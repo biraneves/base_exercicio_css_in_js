a76c1156c249df925ad937a93f4ce4dbaf13be30
Ubirajara Neves <ubirajara.neves@usp.br>
2024-01-31 10:12:33 +0100

    Prettier com novas regras

--- src/containers/ListaVagas/index.tsx
+++ src/containers/ListaVagas/index.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import FormVagas from '../../components/FormVagas'
+import { useState } from 'react';
+import FormVagas from '../../components/FormVagas';
 
-import Vaga from '../../components/Vaga'
+import Vaga from '../../components/Vaga';
 
-import styles from './ListaVagas.module.css'
+import styles from './ListaVagas.module.css';
 
 type Vaga = {
-  id: string
-  titulo: string
-  localizacao: string
-  nivel: string
-  modalidade: string
-  salarioMin: number
-  salarioMax: number
-  requisitos: string[]
-}
+  id: string;
+  titulo: string;
+  localizacao: string;
+  nivel: string;
+  modalidade: string;
+  salarioMin: number;
+  salarioMax: number;
+  requisitos: string[];
+};
 
 const vagas = [
   {
@@ -25,7 +25,7 @@ const vagas = [
     modalidade: 'clt',
     salarioMin: 3000,
     salarioMax: 4500,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 2,
@@ -35,7 +35,7 @@ const vagas = [
     modalidade: 'pj',
     salarioMin: 5000,
     salarioMax: 6500,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 3,
@@ -45,7 +45,7 @@ const vagas = [
     modalidade: 'pj',
     salarioMin: 4000,
     salarioMax: 6000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 4,
@@ -55,7 +55,7 @@ const vagas = [
     modalidade: 'clt',
     salarioMin: 4000,
     salarioMax: 5000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 5,
@@ -65,7 +65,7 @@ const vagas = [
     modalidade: 'clt',
     salarioMin: 7000,
     salarioMax: 8000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 6,
@@ -75,7 +75,7 @@ const vagas = [
     modalidade: 'pj',
     salarioMin: 12000,
     salarioMax: 15000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
   },
   {
     id: 7,
@@ -85,16 +85,16 @@ const vagas = [
     modalidade: 'clt',
     salarioMin: 4000,
     salarioMax: 5000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
-  }
-]
+    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+  },
+];
 
 const ListaVagas = () => {
-  const [filtro, setFiltro] = useState<string>('')
+  const [filtro, setFiltro] = useState<string>('');
 
   const vagasFiltradas = vagas.filter(
-    (x) => x.titulo.toLocaleLowerCase().search(filtro) >= 0
-  )
+    (x) => x.titulo.toLocaleLowerCase().search(filtro) >= 0,
+  );
 
   return (
     <div>
@@ -114,7 +114,7 @@ const ListaVagas = () => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ListaVagas
+export default ListaVagas;
